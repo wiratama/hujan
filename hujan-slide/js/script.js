5df388a3b033d52ca963f200bb0529890943f86a
wiratama <electro_9@ymail.com>
2015-03-05 18:24:23 +0800

    lanjut dirumah

--- hujan-slide/js/script.js
+++ hujan-slide/js/script.js
@@ -1,7 +1,7 @@
 $( document ).ready( function() {
 	
 	var $container = $('#container');	
-	var element_li = ['home','concept','chef','menu','contact'];
+	var element_li = ['home','concept','chef','menu','contact','gallery','farm','reservation','accolades'];
 	
 	$('div.main').css( 'visibility', 'visible' );	
 	$container.stop().scrollTo('div.home');	
@@ -10,13 +10,25 @@ $( document ).ready( function() {
 		var nav_element = 'a.' + el;
 		var destination = 'div.' + el;
 
-		console.log(i);
-		console.log(el);
-		
 		$( nav_element ).click( function( e ) {			
 			$('div.bg').fadeIn( 2500 );				
 			$container.stop().scrollTo( destination, 2500, { easing:'easeInOutExpo' } );			
 			e.preventDefault();
 		});
 	});
+
+	$("#owl-demo").owlCarousel({             
+		navigation : true, // Show next and prev buttons
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem:true
+
+		// "singleItem:true" is a shortcut for:
+		// items : 1, 
+		// itemsDesktop : false,
+		// itemsDesktopSmall : false,
+		// itemsTablet: false,
+		// itemsMobile : false
+	});
+
 });
